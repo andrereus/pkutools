c96920570d89e394869514baf428c8c579590201
Andre Reus <andrereus@users.noreply.github.com>
2020-04-10 20:02:34 +0200

    Remove boilerplate code

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,29 @@
 <template>
   <div id="app">
-    <b-navbar toggleable="sm" type="light" variant="light">
-      <b-navbar-brand to="/">PKU Tools</b-navbar-brand>
+    <b-navbar toggleable="sm" type="dark" variant="dark">
+      <b-container>
+        <b-navbar-brand to="/">PKU Tools</b-navbar-brand>
 
-      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
-      <b-collapse id="nav-collapse" is-nav>
-        <b-navbar-nav>
-          <b-nav-item to="/">Home</b-nav-item>
-          <b-nav-item to="/about">About</b-nav-item>
-          <b-nav-item-dropdown text="Apps" right>
-            <b-dropdown-item href="https://pherechner.de" target="_blank">
-              Phe Rechner <b-icon icon="box-arrow-up-right"></b-icon>
-            </b-dropdown-item>
-            <b-dropdown-item href="https://pkumanager.com" target="_blank">
-              PKU Manager <b-icon icon="box-arrow-up-right"></b-icon>
-            </b-dropdown-item>
-          </b-nav-item-dropdown>
-        </b-navbar-nav>
-      </b-collapse>
+        <b-collapse id="nav-collapse" is-nav>
+          <b-navbar-nav class="ml-auto">
+            <b-nav-item to="/">Home</b-nav-item>
+            <b-nav-item to="/about">About</b-nav-item>
+            <b-nav-item-dropdown text="Apps" right>
+              <b-dropdown-item href="https://pherechner.de/" target="_blank">
+                Phe Rechner <b-icon icon="box-arrow-up-right"></b-icon>
+              </b-dropdown-item>
+              <b-dropdown-item href="https://pkumanager.com/" target="_blank">
+                PKU Manager <b-icon icon="box-arrow-up-right"></b-icon>
+              </b-dropdown-item>
+            </b-nav-item-dropdown>
+          </b-navbar-nav>
+        </b-collapse>
+      </b-container>
     </b-navbar>
-    <router-view />
+    <b-container>
+      <router-view />
+    </b-container>
   </div>
 </template>
-
-<style lang="scss"></style>
