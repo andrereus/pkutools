5e2f5090f969e08cea069fdc91ff41435bced45b
Andre Reus <andrereus@users.noreply.github.com>
2020-04-11 21:44:47 +0200

    Optimize cards

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -10,14 +10,8 @@
     <hr class="my-4" />
 
     <b-card-group deck>
-      <b-card
-        title="Phe Rechner"
-        img-src="../assets/pherechner-app.png"
-        img-alt="Phe Rechner App"
-        img-top
-        style="max-width: 20rem;"
-        class="mb-2"
-      >
+      <b-card img-src="../assets/pherechner-app.png" img-alt="Phe Rechner App" img-top>
+        <b-card-title>Phe Rechner</b-card-title>
         <b-card-text>German PKU app that combines calculator, pen, paper and nutrition table (~ 2017).</b-card-text>
 
         <b-button href="https://pherechner.de/" target="_blank" variant="primary">
@@ -26,14 +20,8 @@
         </b-button>
       </b-card>
 
-      <b-card
-        title="PKU Manager"
-        img-src="../assets/pkumanager-app.png"
-        img-alt="PKU Manager App"
-        img-top
-        style="max-width: 20rem;"
-        class="mb-2"
-      >
+      <b-card img-src="../assets/pkumanager-app.png" img-alt="PKU Manager App" img-top>
+        <b-card-title>PKU Manager</b-card-title>
         <b-card-text>English PKU app for searching, calculating and tracking food (~ 2018).</b-card-text>
 
         <b-button href="https://pkumanager.com/" target="_blank" variant="primary">
@@ -42,14 +30,8 @@
         </b-button>
       </b-card>
 
-      <b-card
-        title="Simple Nutrition Calculator"
-        img-src="../assets/simple-nutrition-calculator-app.png"
-        img-alt="Simple Nutrition Calculator App"
-        img-top
-        style="max-width: 20rem;"
-        class="mb-2"
-      >
+      <b-card img-src="../assets/simple-nutrition-calculator-app.png" img-alt="Simple Nutrition Calculator App" img-top>
+        <b-card-title>Simple Nutrition Calculator</b-card-title>
         <b-card-text>General purpose app for calculating with any nutrient (~ 2019).</b-card-text>
 
         <b-button href="https://simple-nutrition-calculator.com/" target="_blank" variant="primary">
