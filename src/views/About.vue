37e30e3c3f50ac64da8bea4772ff3ea7da0647d7
Andre Reus <andrereus@users.noreply.github.com>
2020-04-19 18:17:03 +0200

    Add AnnounceKit and Hotjar

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,16 +1,6 @@
 <template>
   <div>
-    <h2 class="my-4">Support</h2>
-
-    <hr class="mb-4" />
-
-    <p>Updates, Roadmap, Bugs, FAQs and Contact:</p>
-    <b-button href="https://www.producthero.app/89/pku-tools" target="_blank" variant="primary">
-      Support Center
-      <b-icon icon="box-arrow-up-right"></b-icon>
-    </b-button>
-
-    <h2 class="mt-5 mb-4">About</h2>
+    <h2 class="my-4">About</h2>
 
     <hr class="mb-4" />
 
