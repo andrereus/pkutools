c5869edfb448fe726871335049708291e8bcc0fd
Andre Reus <andrereus@users.noreply.github.com>
2020-04-26 19:51:22 +0200

    Migrate Supplement Counter

--- src/views/SupplementCounter.vue
+++ src/views/SupplementCounter.vue
@@ -1,38 +1,17 @@
 <template>
   <div>
-    <h2 class="my-4">Supplement Counter</h2>
+    <h2>Supplement Counter</h2>
 
-    <b-row class="mb-3">
-      <b-col>
-        <span>I took {{ count }} of {{ max }} supplement{{ max === 1 ? "" : "s" }} today</span>
+    <span>I took {{ count }} of {{ max }} supplements today</span>
+    <v-progress-linear :value="(count * 100) / 3"></v-progress-linear>
 
-        <b-button v-b-modal.amount variant="link" size="sm" class="float-right">
-          <b-icon icon="gear"></b-icon>
-          Amount
-        </b-button>
+    <v-btn v-on:click="count++">Take supplement</v-btn>
+    <v-btn v-on:click="count = 0">Reset</v-btn>
 
-        <b-modal id="amount" title="Amount" ok-only hide-header-close>
-          <label for="amount-input">Amount of supplements per day</label>
-          <b-form-input id="amount-input" v-model="max" type="number"></b-form-input>
-        </b-modal>
-      </b-col>
-    </b-row>
+    <br />
 
-    <b-progress :value="count" :max="max" class="mb-4"></b-progress>
-
-    <b-row>
-      <b-col>
-        <b-button v-on:click="count++" variant="success" size="sm">
-          <b-icon icon="plus"></b-icon>
-          Take supplement
-        </b-button>
-
-        <b-button v-on:click="count = 0" variant="outline-danger" size="sm" class="float-right">
-          <b-icon icon="trash"></b-icon>
-          Reset
-        </b-button>
-      </b-col>
-    </b-row>
+    <label for="amount-input">Amount of supplements per day</label>
+    <v-text-field id="amount-input" v-model="max" type="number"></v-text-field>
   </div>
 </template>
 
