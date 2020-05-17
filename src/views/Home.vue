887b9bbe3c3484dd9b6d29b84edcefbff2bfb572
Andre Reus <andrereus@users.noreply.github.com>
2020-05-17 14:18:55 +0200

    Update translation alert

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,14 +8,14 @@
 
     <v-row justify="center">
       <v-col cols="12" md="10" lg="8" xl="6">
+        <v-alert dense type="warning" class="mb-7">{{ $t("home.translation") }}</v-alert>
+
         <p>{{ $t("app.description") }}</p>
 
         <v-btn depressed to="/phe-calculator" color="primary" class="mr-3 mt-3">{{ $t("phe-calculator.title") }}</v-btn>
         <v-btn depressed to="/phe-search" color="primary" class="mr-3 mt-3">{{ $t("phe-search.title") }}</v-btn>
         <v-btn depressed to="/amino-counter" color="primary" class="mr-3 mt-3">{{ $t("amino-counter.title") }}</v-btn>
         <v-btn disabled class="mt-3">{{ $t("home.progress") }}</v-btn>
-
-        <v-alert type="warning" class="mt-6">{{ $t("home.translation") }}</v-alert>
       </v-col>
     </v-row>
   </div>
