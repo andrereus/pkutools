c4e8f0f0aa3fa6b2a23ef443ff12eb682b484b83
Andre Reus <andrereus@users.noreply.github.com>
2020-05-25 21:41:25 +0200

    Add food illustration

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -10,6 +10,8 @@
       <v-col cols="12" md="10" lg="8" xl="6">
         <p>{{ $t("app.description") }}</p>
 
+        <v-img src="../assets/the-munchies.png" alt="Food Illustration" class="mt-6 mb-3"></v-img>
+
         <v-btn depressed to="/phe-calculator" color="primary" class="mr-3 mt-3">{{ $t("phe-calculator.title") }}</v-btn>
         <v-btn depressed to="/phe-search" color="primary" class="mr-3 mt-3">{{ $t("phe-search.title") }}</v-btn>
         <v-btn depressed to="/amino-counter" color="primary" class="mr-3 mt-3">{{ $t("amino-counter.title") }}</v-btn>
