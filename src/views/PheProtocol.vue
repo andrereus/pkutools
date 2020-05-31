d74400384366a0a0fd250696ca8078a763045d1d
Andre Reus <andrereus@users.noreply.github.com>
2020-05-31 14:51:20 +0200

    Remove auth

--- src/views/PheProtocol.vue
+++ src/views/PheProtocol.vue
@@ -8,31 +8,8 @@
 
     <v-row justify="center">
       <v-col cols="12" md="10" lg="8" xl="6">
-        <v-btn depressed disabled color="primary" @click="authenticate">Sign in with Google</v-btn>
+        <p>{{ $t("phe-protocol.title") }}</p>
       </v-col>
     </v-row>
   </div>
 </template>
-
-<script>
-import firebase from "firebase/app";
-import "firebase/auth";
-
-export default {
-  methods: {
-    authenticate: function() {
-      const provider = new firebase.auth.GoogleAuthProvider();
-      firebase.auth().signInWithRedirect(provider);
-      firebase
-        .auth()
-        .getRedirectResult()
-        .then(function(result) {
-          console.log(result);
-        })
-        .catch(function(error) {
-          console.log(error);
-        });
-    }
-  }
-};
-</script>
