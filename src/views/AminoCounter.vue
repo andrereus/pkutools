7d8d3db8979e1d511d5596df10c7b5a74d1cd8a3
Andre Reus <andrereus@users.noreply.github.com>
2020-06-12 15:42:06 +0200

    Optimize Amino Counter

--- src/views/AminoCounter.vue
+++ src/views/AminoCounter.vue
@@ -10,21 +10,41 @@
       <v-col cols="12" md="10" lg="8" xl="6">
         <p>{{ $t("amino-counter.description") }}</p>
 
-        <v-text-field
-          dense
-          outlined
-          :label="$t('amino-counter.amount')"
-          v-model.number="max"
-          type="number"
-          class="mt-8"
-        ></v-text-field>
-
-        <v-progress-linear :value="(count * 100) / max" height="40" class="white--text mb-4">
+        <v-progress-linear :value="(count * 100) / max" height="40" class="white--text mt-6 mb-4">
           {{ $t("amino-counter.progress", { count, max }) }}
         </v-progress-linear>
 
         <v-btn depressed @click="count++" color="primary" class="mr-3 mt-3">{{ $t("amino-counter.take") }}</v-btn>
-        <v-btn depressed @click="count = 0" class="mt-3">{{ $t("amino-counter.reset") }}</v-btn>
+        <v-btn depressed @click="count = 0" class="mr-3 mt-3">{{ $t("amino-counter.reset") }}</v-btn>
+
+        <v-dialog v-model="dialog" max-width="500px">
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn depressed v-bind="attrs" v-on="on" class="mr-3 mt-3">
+              {{ $t("amino-counter.settings") }}
+            </v-btn>
+          </template>
+
+          <v-card>
+            <v-card-title>
+              <span class="headline">{{ $t("amino-counter.settings") }}</span>
+            </v-card-title>
+
+            <v-card-text>
+              <v-text-field
+                outlined
+                :label="$t('amino-counter.amount')"
+                v-model.number="max"
+                type="number"
+                class="mt-6"
+              ></v-text-field>
+            </v-card-text>
+
+            <v-card-actions class="mt-n6">
+              <v-spacer></v-spacer>
+              <v-btn depressed @click="dialog = false">{{ $t("amino-counter.ok") }}</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
       </v-col>
     </v-row>
   </div>
@@ -33,6 +53,7 @@
 <script>
 export default {
   data: () => ({
+    dialog: false,
     count: 0,
     max: 3
   }),
