107d217a8089c109fcb82dd928f6a33a52dd226a
Andre Reus <andrereus@users.noreply.github.com>
2020-08-04 15:59:02 +0200

    Revert "Add accent color"
    This reverts commit b5e6cbc2fccb7f9aabbb85e7513c67233d876d9b.

--- src/App.vue
+++ src/App.vue
@@ -243,7 +243,7 @@ export default {
 }
 
 .bottom-nav--active {
-  border-bottom: 2px solid #3498db;
+  border-bottom: 2px solid #000;
 }
 
 .theme--dark {
@@ -258,6 +258,10 @@ export default {
     @media (min-width: 1264px) {
       background-color: transparent;
     }
+  }
+
+  .bottom-nav--active {
+    border-bottom: 2px solid #fff;
   }
 }
 </style>
