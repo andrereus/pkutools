0211bd156bcb1fc482f0d88bb40588ca7b508d42
Andre Reus <andrereus@users.noreply.github.com>
2020-08-10 21:39:23 +0200

    Remove Phe Log from navigation

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -12,7 +12,7 @@
 
         <v-btn depressed to="/phe-calculator" color="primary" class="mr-3 mt-3">{{ $t("phe-calculator.title") }}</v-btn>
         <v-btn depressed to="/phe-search" color="primary" class="mr-3 mt-3">{{ $t("phe-search.title") }}</v-btn>
-        <v-btn depressed to="/phe-log" color="primary" class="mr-3 mt-3">{{ $t("phe-log.title") }}</v-btn>
+        <!--        <v-btn depressed to="/phe-log" color="primary" class="mr-3 mt-3">{{ $t("phe-log.title") }}</v-btn>-->
         <v-btn depressed to="/amino-counter" color="primary" class="mr-3 mt-3">{{ $t("amino-counter.title") }}</v-btn>
 
         <v-img src="../assets/eating-together.svg" alt="Food Illustration" class="mt-8 illustration"></v-img>
