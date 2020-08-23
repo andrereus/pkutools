db0e0cffe34f9a5d90cd492526435747a401f604
Andre Reus <andrereus@users.noreply.github.com>
2020-08-23 20:35:37 +0200

    Offline data loss prevention

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -1,6 +1,8 @@
 /* eslint-disable no-console */
 
 import { register } from "register-service-worker";
+import * as firebase from "firebase/app";
+import "firebase/auth";
 
 if (process.env.NODE_ENV === "production") {
   register(`${process.env.BASE_URL}service-worker.js`, {
@@ -23,6 +25,12 @@ if (process.env.NODE_ENV === "production") {
     },
     offline() {
       console.log("No internet connection found. App is running in offline mode.");
+      firebase
+        .auth()
+        .signOut()
+        .catch(error => {
+          console.log(error);
+        });
     },
     error(error) {
       console.error("Error during service worker registration:", error);
