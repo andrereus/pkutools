38ad19059a256231c04e235fdb26b6f7a46e299a
Andre Reus <andrereus@users.noreply.github.com>
2020-10-03 09:20:24 +0200

    Keyboard close

--- src/views/PheSearch.vue
+++ src/views/PheSearch.vue
@@ -17,14 +17,13 @@
         <v-text-field
           :value="search"
           @keyup="search = $event.target.value"
-          append-icon="mdi-magnify"
+          append-icon="mdi-keyboard-close"
           :label="$t('phe-search.search')"
           filled
           single-line
           @click:append="closeKeys"
           ref="foodSearch"
           autofocus
-          clearable
         ></v-text-field>
 
         <v-data-table
