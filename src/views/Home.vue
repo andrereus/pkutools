865acca93a15e60e6240dd58290717386d7cc60a
Andre Reus <andrereus@users.noreply.github.com>
2020-10-27 14:33:47 +0100

    Change chart height

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -52,7 +52,7 @@
             <v-card-text>
               <p>{{ $t("phe-diary.title") }}</p>
               <p v-if="graph.length < 2">{{ $t("phe-diary.chart") }}</p>
-              <v-sparkline :value="graph" fill smooth class="mt-9"></v-sparkline>
+              <v-sparkline :value="graph" fill smooth height="125"></v-sparkline>
             </v-card-text>
           </v-card>
         </div>
