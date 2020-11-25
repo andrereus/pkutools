1329ce65d0c9e4ce0ebf7fd56080152100693cff
Andre Reus <andrereus@users.noreply.github.com>
2020-11-25 22:25:18 +0100

    Change login homescreen

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -51,30 +51,31 @@
           </v-card>
         </div>
 
-        <v-btn depressed to="/phe-search" color="primary" class="mr-3 mb-3">{{ $t("phe-search.title") }}</v-btn>
-        <v-btn depressed to="/phe-calculator" color="primary" class="mr-3 mb-3">{{ $t("phe-calculator.title") }}</v-btn>
-        <v-btn depressed to="/protein-calculator" color="primary" class="mr-3 mb-3">
-          {{ $t("protein-calculator.title") }}
-        </v-btn>
-        <v-btn depressed to="/own-food" color="primary" class="mr-3 mb-3" v-if="userIsAuthenticated">
-          {{ $t("own-food.title") }}
-        </v-btn>
+        <div v-if="!userIsAuthenticated">
+          <v-btn depressed to="/phe-search" color="primary" class="mr-3 mb-3">{{ $t("phe-search.title") }}</v-btn>
+          <v-btn depressed to="/phe-calculator" color="primary" class="mr-3 mb-3">
+            {{ $t("phe-calculator.title") }}
+          </v-btn>
+          <v-btn depressed to="/protein-calculator" color="primary" class="mr-3 mb-3">
+            {{ $t("protein-calculator.title") }}
+          </v-btn>
 
-        <v-menu offset-y v-if="!userIsAuthenticated">
-          <template v-slot:activator="{ on, attrs }">
-            <v-btn text class="mr-3 mb-3" v-bind="attrs" v-on="on">
-              {{ $t("home.more") }}
-            </v-btn>
-          </template>
-          <v-list>
-            <v-list-item @click="signInGoogle">
-              <span>
-                <v-icon>mdi-google</v-icon>
-                {{ $t("app.signin-google") }}
-              </span>
-            </v-list-item>
-          </v-list>
-        </v-menu>
+          <v-menu offset-y>
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn text class="mr-3 mb-3" v-bind="attrs" v-on="on">
+                {{ $t("home.more") }}
+              </v-btn>
+            </template>
+            <v-list>
+              <v-list-item @click="signInGoogle">
+                <span>
+                  <v-icon>mdi-google</v-icon>
+                  {{ $t("app.signin-google") }}
+                </span>
+              </v-list-item>
+            </v-list>
+          </v-menu>
+        </div>
 
         <v-img src="../assets/eating-together.svg" alt="Food Illustration" class="mt-4 illustration"></v-img>
 
