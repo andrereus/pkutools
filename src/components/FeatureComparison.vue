5f43f19a69f6c969c3e205e9b75461c1102d17e7
Andre Reus <andrereus@users.noreply.github.com>
2021-01-25 00:33:07 +0100

    Revert "Correct checkmark"
    This reverts commit d3f1016ebce38883dbbfb2e9116034eb5657b7d0.

--- src/components/FeatureComparison.vue
+++ src/components/FeatureComparison.vue
@@ -132,7 +132,7 @@ export default {
         de: "Windows",
         pkutools: 1,
         pkumanager: 3,
-        pherechner: 2
+        pherechner: 3
       },
       {
         icon: "mdi-desktop-mac",
