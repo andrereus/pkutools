6e33aeafb8cf14a223865b4dfdc730e8b3c8b422
Andre Reus <andrereus@users.noreply.github.com>
2021-01-30 11:17:27 +0100

    Update help page

--- src/views/Help.vue
+++ src/views/Help.vue
@@ -39,77 +39,106 @@
 
         <h2 class="headline my-6">{{ $t("help.faq") }}</h2>
 
-        <h3>{{ $t("help.install") }}</h3>
-        <p>{{ $t("help.install-p1") }}</p>
+        <v-expansion-panels>
+          <v-expansion-panel>
+            <v-expansion-panel-header>{{ $t("help.install") }}</v-expansion-panel-header>
 
-        <v-btn depressed outlined color="primary" @click="refreshPage" class="mr-3 mb-3">
-          <v-icon left>mdi-refresh</v-icon>
-          {{ $t("app.refresh") }}
-        </v-btn>
+            <v-expansion-panel-content>
+              <p>{{ $t("help.install-p1") }}</p>
 
-        <v-btn depressed color="primary" @click="installPWA" class="mr-3 mb-3">
-          <v-icon left>mdi-download</v-icon>
-          {{ $t("app.install") }}
-        </v-btn>
+              <v-btn depressed outlined color="primary" @click="refreshPage" class="mr-3 mb-3">
+                <v-icon left>mdi-refresh</v-icon>
+                {{ $t("app.refresh") }}
+              </v-btn>
 
-        <pwa-install :installbuttontext="$t('app.install-pwa')" usecustom></pwa-install>
+              <v-btn depressed color="primary" @click="installPWA" class="mr-3 mb-3">
+                <v-icon left>mdi-download</v-icon>
+                {{ $t("app.install") }}
+              </v-btn>
 
-        <span class="mr-3">{{ $t("help.install-p3") }}</span>
+              <pwa-install :installbuttontext="$t('app.install-pwa')" usecustom></pwa-install>
 
-        <a
-          href="https://play.google.com/store/apps/details?id=com.pkutools.twa&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1"
-        >
-          <img
-            alt="Get it on Google Play"
-            src="https://play.google.com/intl/en_us/badges/static/images/badges/en_badge_web_generic.png"
-            class="gplay-btn ml-n2 mb-3"
-          />
-        </a>
+              <span class="mr-3">{{ $t("help.install-p3") }}</span>
 
-        <p>
-          <v-icon>mdi-information-outline</v-icon>
-          {{ $t("help.install-p2") }}
-        </p>
+              <a
+                href="https://play.google.com/store/apps/details?id=com.pkutools.twa&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1"
+              >
+                <img
+                  alt="Get it on Google Play"
+                  src="https://play.google.com/intl/en_us/badges/static/images/badges/en_badge_web_generic.png"
+                  class="gplay-btn ml-n2 mb-3"
+                />
+              </a>
 
-        <h3>{{ $t("help.sign-in") }}</h3>
-        <p>{{ $t("help.sign-in-p1") }}</p>
-        <p>{{ $t("help.sign-in-p2") }}</p>
+              <p>
+                <v-icon>mdi-information-outline</v-icon>
+                {{ $t("help.install-p2") }}
+              </p>
+            </v-expansion-panel-content>
+          </v-expansion-panel>
 
-        <h3>{{ $t("help.roadmap") }}</h3>
-        <p>
-          {{ $t("help.roadmap-p1") }}
-          <a href="https://trello.com/b/jJiZ0tJm" target="_blank">https://trello.com/b/jJiZ0tJm</a>
-        </p>
+          <v-expansion-panel>
+            <v-expansion-panel-header>{{ $t("help.sign-in") }}</v-expansion-panel-header>
 
-        <h3>{{ $t("help.support") }}</h3>
-        <p>
-          {{ $t("help.support-p1") }}
-          <a href="https://www.buymeacoffee.com/andrereus" target="_blank">https://www.buymeacoffee.com/andrereus</a>
-          😉
-        </p>
+            <v-expansion-panel-content>
+              <p>{{ $t("help.sign-in-p1") }}</p>
+              <p>{{ $t("help.sign-in-p2") }}</p>
+            </v-expansion-panel-content>
+          </v-expansion-panel>
 
-        <h2 class="headline my-6">{{ $t("help.feedback") }}</h2>
+          <v-expansion-panel>
+            <v-expansion-panel-header>{{ $t("help.roadmap") }}</v-expansion-panel-header>
+
+            <v-expansion-panel-content>
+              <p>
+                {{ $t("help.roadmap-p1") }}
+                <a href="https://trello.com/b/jJiZ0tJm" target="_blank">https://trello.com/b/jJiZ0tJm</a>
+              </p>
+            </v-expansion-panel-content>
+          </v-expansion-panel>
+
+          <v-expansion-panel>
+            <v-expansion-panel-header>{{ $t("help.support") }}</v-expansion-panel-header>
+
+            <v-expansion-panel-content>
+              <p>
+                {{ $t("help.support-p1") }}
+                <a href="https://www.buymeacoffee.com/andrereus" target="_blank">
+                  https://www.buymeacoffee.com/andrereus
+                </a>
+                😉
+              </p>
+            </v-expansion-panel-content>
+          </v-expansion-panel>
+        </v-expansion-panels>
+
+        <h2 class="headline my-6">{{ $t("help.more") }}</h2>
+
+        <!-- prettier-ignore -->
+        <p><v-icon left>mdi-email</v-icon>{{ part1 }}@<span class="hidden">null</span>{{ part2 }}.{{ part3 }}</p>
 
         <v-btn
           v-if="this.$i18n.locale === 'en'"
           depressed
+          outlined
           href="https://andre.getform.com/q0ko3"
           color="primary"
           class="mr-3 mb-3"
         >
-          <v-icon left>mdi-email</v-icon>
-          {{ $t("help.contact") }}
+          <v-icon left>mdi-comment-quote-outline</v-icon>
+          {{ $t("help.feedback") }}
         </v-btn>
 
         <v-btn
           v-if="this.$i18n.locale === 'de'"
           depressed
+          outlined
           href="https://andre.getform.com/69zrd"
           color="primary"
           class="mr-3 mb-3"
         >
-          <v-icon left>mdi-email</v-icon>
-          {{ $t("help.contact") }}
+          <v-icon left>mdi-comment-quote-outline</v-icon>
+          {{ $t("help.feedback") }}
         </v-btn>
       </v-col>
     </v-row>
@@ -126,6 +155,11 @@ export default {
       meta: [{ name: "description", content: this.$t("help.description") }]
     };
   },
+  data: () => ({
+    part1: "heypku",
+    part2: "gmail",
+    part3: "com"
+  }),
   methods: {
     refreshPage() {
       location.reload();
@@ -138,6 +172,10 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.hidden {
+  display: none;
+}
+
 .v-btn {
   text-transform: none;
 }
