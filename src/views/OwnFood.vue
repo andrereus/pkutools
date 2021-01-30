068c495655741c7c41e0af172cce2a372df0b31c
Andre Reus <andrereus@users.noreply.github.com>
2021-01-31 00:01:07 +0100

    Food Icon Picker in Own Food V1

--- src/views/OwnFood.vue
+++ src/views/OwnFood.vue
@@ -68,7 +68,39 @@
               </v-card-title>
 
               <v-card-text>
-                <v-text-field filled label="Name" v-model="editedItem.name" class="mt-6"></v-text-field>
+                <v-text-field filled label="Name" v-model="editedItem.name" class="mt-6">
+                  <template v-slot:append-outer>
+                    <v-menu offset-y>
+                      <template v-slot:activator="{ on, attrs }">
+                        <v-btn fab small depressed v-bind="attrs" v-on="on" class="mt-n2">
+                          <img
+                            :src="publicPath + 'img/food-icons/' + editedItem.icon + '.svg'"
+                            v-if="editedItem.icon !== undefined"
+                            width="40"
+                            class="food-icon"
+                          />
+                          <img
+                            :src="publicPath + 'img/food-icons/organic-food.svg'"
+                            v-if="editedItem.icon === undefined"
+                            width="40"
+                            class="food-icon"
+                          />
+                        </v-btn>
+                      </template>
+                      <v-card max-width="300" max-height="250" class="overflow-y-auto">
+                        <span v-for="(item, index) in foodIcons" :key="index" class="px-1">
+                          <img
+                            :src="publicPath + 'img/food-icons/' + item.svg + '.svg'"
+                            v-if="item.svg !== undefined"
+                            width="40"
+                            class="food-icon pick-icon"
+                            @click="editedItem.icon = item.svg"
+                          />
+                        </span>
+                      </v-card>
+                    </v-menu>
+                  </template>
+                </v-text-field>
                 <v-text-field
                   filled
                   :label="$t('own-food.phe')"
@@ -141,6 +173,7 @@ import { mapState } from "vuex";
 import * as firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/database";
+import foodIcons from "../components/data/food-icons.json";
 
 export default {
   metaInfo() {
@@ -172,7 +205,8 @@ export default {
       phe: null
     },
     weight: 100,
-    offlineInfo: false
+    offlineInfo: false,
+    foodIcons
   }),
   methods: {
     signInGoogle() {
@@ -302,6 +336,10 @@ export default {
 
 .food-icon {
   vertical-align: bottom;
+}
+
+.pick-icon {
+  cursor: pointer;
 }
 
 .theme--light.v-data-table {
