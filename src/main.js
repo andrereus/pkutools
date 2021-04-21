ac078459d7a86c9cea83e494ac66c301c5ab1f6c
Andre Reus <andrereus@users.noreply.github.com>
2021-04-21 15:56:13 +0200

    Optimize prerendering

--- src/main.js
+++ src/main.js
@@ -8,10 +8,14 @@ import i18n from "./plugins/i18n";
 
 Vue.config.productionTip = false;
 
-new Vue({
+const root = new Vue({
   router,
   store,
   vuetify,
   i18n,
   render: h => h(App)
-}).$mount("#app");
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  root.$mount("#app");
+});
