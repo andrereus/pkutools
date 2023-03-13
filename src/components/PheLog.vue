ced80e20dc01319e8ad60b7b162aae45b8998d06
Andre Reus <andrereus@users.noreply.github.com>
2023-03-13 16:10:12 +0100

    Fix console error

--- src/components/PheLog.vue
+++ src/components/PheLog.vue
@@ -140,14 +140,14 @@
       </v-data-table>
 
       <v-progress-linear
-        :value="(pheResult * 100) / (settings.maxPhe || 0)"
+        :value="(pheResult * 100) / (settings?.maxPhe || 0)"
         height="15"
         class="mt-8"
         rounded
       ></v-progress-linear>
 
       <p class="title font-weight-regular mt-6">{{ $t("phe-log.total") }}: {{ pheResult }} mg Phe</p>
-      <p class="mt-n2 mb-6">{{ $t("phe-log.remaining") }}: {{ (settings.maxPhe || 0) - pheResult }} mg Phe</p>
+      <p class="mt-n2 mb-6">{{ $t("phe-log.remaining") }}: {{ (settings?.maxPhe || 0) - pheResult }} mg Phe</p>
 
       <v-btn depressed rounded color="success" class="mr-3 mb-3" @click="saveResult">
         {{ $t("phe-log.save-day") }}
