5d9d60d95c8b2e5c5aa64386cddca31897e44447
Andre Reus <andrereus@users.noreply.github.com>
2023-03-27 11:43:35 +0200

    Add barcode scanner note

--- src/views/BarcodeScanner.vue
+++ src/views/BarcodeScanner.vue
@@ -8,6 +8,8 @@
 
     <v-row justify="center">
       <v-col cols="12" md="10" lg="8" xl="6">
+        <p v-if="userIsAuthenticated" class="mb-6">{{ $t("barcode-scanner.description") }}</p>
+
         <v-dialog v-model="dialog" max-width="500px">
           <template v-slot:activator="{ on, attrs }">
             <v-btn depressed rounded color="primary" v-bind="attrs" v-on="on" class="mr-3 mb-3">
