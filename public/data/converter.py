658d3c1e039940e5ed71a51080d11cb96a6fd3a2
Andre Reus <andrereus@users.noreply.github.com>
2023-03-29 23:01:40 +0200

    Python utilities

---
+++ public/data/converter.py
@@ -0,0 +1,53 @@
+import codecs
+import json
+import csv
+import os
+
+def json_to_csv(input_file, output_file):
+    with open(input_file, 'r', encoding='utf-8') as jsonfile:
+        data = json.load(jsonfile)
+
+    with open(output_file, 'w', newline='', encoding='utf-8') as csvfile:
+        csvfile.write(codecs.BOM_UTF8.decode('utf-8'))
+        fieldnames = ['name', 'phe']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+        for row in data:
+            writer.writerow(row)
+
+def csv_to_json(input_file, output_file):
+    csv_data = []
+
+    with open(input_file, 'r', newline='', encoding='utf-8') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            csv_data.append(row)
+
+    json_data = json.dumps(csv_data, indent=4)
+
+    with open(output_file, 'w', encoding='utf-8') as jsonfile:
+        jsonfile.write(json_data)
+
+def get_output_filename(input_file, output_extension):
+    base_filename, input_extension = os.path.splitext(input_file)
+    output_file = base_filename + output_extension
+    i = 1
+
+    while os.path.exists(output_file):
+        output_file = f"{base_filename}_{i}{output_extension}"
+        i += 1
+
+    return output_file
+
+input_file = input("Enter the file name (including extension) you want to convert: ")
+
+if input_file.endswith(".json"):
+    output_file = get_output_filename(input_file, ".csv")
+    json_to_csv(input_file, output_file)
+    print(f"JSON data converted to CSV: {output_file}")
+elif input_file.endswith(".csv"):
+    output_file = get_output_filename(input_file, ".json")
+    csv_to_json(input_file, output_file)
+    print(f"CSV data converted to JSON: {output_file}")
+else:
+    print("Unsupported file type. Please provide a JSON or CSV file.")
---
+++ public/img/get_icons.py
@@ -0,0 +1,15 @@
+import os
+
+def get_file_names_without_extension(folder_path):
+    file_names = []
+    for file in os.listdir(folder_path):
+        if os.path.isfile(os.path.join(folder_path, file)):
+            file_name_without_extension, _ = os.path.splitext(file)
+            file_names.append(file_name_without_extension)
+    return file_names
+
+folder_path = "food-icons"  # Replace this with the path to your folder
+file_names_without_extension = get_file_names_without_extension(folder_path)
+
+for file_name in file_names_without_extension:
+    print(file_name)
