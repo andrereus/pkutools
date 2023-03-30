fefb04aa3551598cf65c3c4e2a04b883a9d9a56b
Andre Reus <andrereus@users.noreply.github.com>
2023-03-30 22:12:36 +0200

    Experimenting with OpenAI API

--- public/img/get_icons.py
+++ public/img/get_icons.py
@@ -11,5 +11,17 @@ def get_file_names_without_extension(folder_path):
 folder_path = "food-icons"  # Replace this with the path to your folder
 file_names_without_extension = get_file_names_without_extension(folder_path)
 
-comma_separated_file_names = ", ".join(file_names_without_extension)
+print("Choose output format:")
+print("1. Comma-separated list (no quotes)")
+print("2. Comma-separated list (with quotes)")
+choice = int(input("Enter your choice (1 or 2): "))
+
+if choice == 1:
+    comma_separated_file_names = ", ".join(file_names_without_extension)
+elif choice == 2:
+    comma_separated_file_names = ', '.join(f'"{file_name}"' for file_name in file_names_without_extension)
+else:
+    print("Invalid choice. Please enter either 1 or 2.")
+    exit()
+
 print(comma_separated_file_names)
